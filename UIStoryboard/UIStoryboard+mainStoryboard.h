0d4507849dd957623bd2193f19510d6ad6da9348
Gustavo Graña <gustavoggs@gmail.com>
2013-06-24 15:55:59 -0300

    Update UIStoryboard+mainStoryboard.h

--- UIStoryboard/UIStoryboard+mainStoryboard.h
+++ UIStoryboard/UIStoryboard+mainStoryboard.h
@@ -1,9 +1,7 @@
 //
 //  UIStoryboard+mainStoryboard.h
-//  SAG20130425
 //
 //  Created by Gustavo Grana on 6/24/13.
-//  Copyright (c) 2013 Conrad Caine. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
